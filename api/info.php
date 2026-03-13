bad7f228fdf4fb3a185a1ac75e8b24995489194e
daffarizki190 <dafarizki209@gmail.com>
2026-03-14 04:44:22 +0700

    Debug: Add isolated API info function

---
+++ api/info.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Isolated PHP Runtime Test</h1>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+if (function_exists('mongodb_driver_version')) {
+    echo "<p>MongoDB Extension is LOADED.</p>";
+} else {
+    echo "<p>MongoDB Extension is NOT LOADED.</p>";
+}
+echo "<p>Hello, Vercel!</p>";
